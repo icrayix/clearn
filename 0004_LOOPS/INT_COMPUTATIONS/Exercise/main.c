4e76ccef05f6b9c83ff1efa939a32817da65979d
icrayix <icrayix@gmail.com>
2026-01-05 14:46:25 +0100

    loops task

--- 0004_LOOPS/INT_COMPUTATIONS/Exercise/main.c
+++ 0004_LOOPS/INT_COMPUTATIONS/Exercise/main.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -10,16 +11,20 @@ int main()
 
     // sum of digits
     uint32_t numDigits = 0;
-
-    // CODE HERE //
-
+    uint32_t temp = inputNumber;
+    while (temp > 0) {
+        temp /= 10;
+        numDigits++;
+    }
     printf("sum of digits: %u\n", numDigits);
 
     // cross sum
     uint32_t crossSum = 0;
-    
-    // CODE HERE //
-
+    temp = inputNumber;
+    for (size_t i = 0; i < numDigits; i++) {
+        crossSum += temp % 10;
+        temp /= 10;
+    }
     printf("crossSum: %u\n", crossSum);
 
     return 0;

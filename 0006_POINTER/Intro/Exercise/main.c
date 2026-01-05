66f39bbe368b946dd80dfb3334fac0663ad12153
icrayix <icrayix@gmail.com>
2026-01-05 16:36:40 +0100

    00006 pointer intro task

--- 0006_POINTER/Intro/Exercise/main.c
+++ 0006_POINTER/Intro/Exercise/main.c
@@ -2,7 +2,20 @@
 
 int main()
 {
-    // YOUR CODE HERE //
+    int aValue = 42;
+    printf("Initial value:\t\t\t\t\t%d\n", aValue);
+
+    int *myPointer = &aValue;
+    printf("Value through pointer:\t\t\t\t%d\n", *myPointer);
+    *myPointer = 187;
+    printf("Modified value of aValue through myPointer:\t%d\n", *myPointer);
+
+    int *yourPointer = myPointer;
+    *yourPointer = 69;
+
+    printf("Address of aValue:\t\t\t\t%p\n",  &aValue);
+    printf("Address of aValue:\t\t\t\t%p\n",  &myPointer);
+    printf("Address of aValue:\t\t\t\t%p\n",  &yourPointer);
 
     return 0;
 }
